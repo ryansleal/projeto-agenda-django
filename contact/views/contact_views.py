972b0493ec53fbc94ae9079cdb3df596d1874fa1
Ryan Santana <ryan.santana.leal@outlook.com>
2024-05-24 10:28:59 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -10,6 +10,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -25,8 +26,11 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True
     )
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
