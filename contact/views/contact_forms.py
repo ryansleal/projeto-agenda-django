ae61e57d45068790a1e85fd18cd9bfebf3a45a31
Ryan Santana <ryan.santana.leal@outlook.com>
2024-05-29 09:55:58 -0300

    Movendo o ContactForm para forms.py

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,37 +1,6 @@
-from django import forms
-from django.core.exceptions import ValidationError
 from django.shortcuts import render
 
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-
-        self.add_error(
-            None,
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
