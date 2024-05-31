0fcfc876738c0b2499b119c6b775f9391ca356a4
Ryan Santana <ryan.santana.leal@outlook.com>
2024-05-31 10:57:54 -0300

    Criando a view, url e template para "delete" (apagar contatos)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -72,3 +72,29 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    site_title = f'{contact.first_name} {contact.last_name} - '
+
+    context = {
+        'contact': contact,
+        'site_title': site_title,
+        'confirmation': confirmation,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
